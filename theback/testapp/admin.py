31bba01d29a2234cad17bdfa2726a017aaad4a60
Milan Sastry <milansastry@dynamic-oit-ip4-wifirestricted03-10-49-116-253.princeton.edu>
2024-07-31 19:09:00 -0400

    delete

--- theback/testapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import MentorProfile
-from .models import UserProfile
-
-admin.site.register(MentorProfile)
-admin.site.register(UserProfile)
-
-
-# Register your models here.
--- theback/testapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MEdbackendConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'testapp'
